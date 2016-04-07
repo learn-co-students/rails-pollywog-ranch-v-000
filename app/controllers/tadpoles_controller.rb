3146f1da4102ca958ca8f2d17e74ebcf4feb9b65
shinyrachel <rachel.kory@gmail.com>
2016-04-07 17:08:46 -0400

    tadpole controller spec passing

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,7 +1,15 @@
 class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
-  # add your metamorphosize action here
+  def metamorphosize
+    @frog = Frog.new(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
+    if @frog.save
+      @tadpole.destroy
+      redirect_to frog_path(@frog)
+    else
+      redirect to tadpole_path(@tadpole), alert: "Error metamorphosizing tadpole into frog; please try again"
+    end
+  end
   
   def index
     @tadpoles = Tadpole.all
