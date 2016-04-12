0758a3f08a029edc230ca73db5b45cbf39fe4593
Brian Holland <brian.holland@proatria.com>
2016-04-11 23:52:43 -0500

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,16 @@
 class Tadpole < ActiveRecord::Base
   # code goes here
+  belongs_to :frog
+  delegate :pond, to: :frog, allow_nil:false
+
+  def metamorphosize
+    @frog = Frog.new
+    @frog.name = self.name
+    @frog.color = self.color
+    @frog.pond_id = self.frog.pond.id
+    if @frog.save
+      self.destroy
+    end
+    @frog
+  end
 end
