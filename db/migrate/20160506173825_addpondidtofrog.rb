2b4dd5e6ec6ca46857b26b347bfa6b7272ab4044
tejbans <singh.tejbans@gmail.com>
2016-05-06 12:43:59 -0700

    Done.

---
+++ db/migrate/20160506173825_addpondidtofrog.rb
@@ -0,0 +1,5 @@
+class Addpondidtofrog < ActiveRecord::Migration
+  def change
+    add_column :frogs, :pond_id, :integer
+  end
+end
---
+++ db/migrate/20160506174332_addfrogidtotadpole.rb
@@ -0,0 +1,5 @@
+class Addfrogidtotadpole < ActiveRecord::Migration
+  def change
+    add_column :tadpoles, :frog_id, :integer
+  end
+end
