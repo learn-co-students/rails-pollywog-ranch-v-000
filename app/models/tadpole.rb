056cd7f1ede9003d4a1ee47ebd7845a9722b9576
seanliu93 <sean93liu@gmail.com>
2016-05-07 00:58:57 -0400

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,14 @@
 class Tadpole < ActiveRecord::Base
   # code goes here
+  belongs_to :frog
+  delegate :pond, to: :frog, :allow_nil => true
+
+  def metamorphosize
+    name = self.name
+    color = self.color
+    pond_id = self.pond.id
+    self.destroy
+    new_frog = Frog.create(name: name, color: color, pond_id: pond_id)
+    new_frog
+  end
 end
