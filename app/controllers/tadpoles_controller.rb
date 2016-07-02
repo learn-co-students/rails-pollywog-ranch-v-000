ee78043e3d2a1c832da9333b253c42cfd6001ff7
interestinall <justin.lefurjah@gmail.com>
2016-07-02 08:14:04 -0500

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,7 +2,7 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
   # add your metamorphosize action here
-  
+
   def index
     @tadpoles = Tadpole.all
   end
@@ -45,6 +45,16 @@ class TadpolesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tadpoles_url, notice: 'Tadpole was successfully destroyed.' }
     end
+  end
+
+  def metamorphosize
+   @frog = Frog.new(color: @tadpole.color, name: @tadpole.name, pond_id: @tadpole.pond.id)
+     if @frog.save
+       @tadpole.destroy
+       redirect_to frog_path(@frog)
+     else
+       redirect_to tadpole_path(@tadpole)
+     end
   end
 
   private
