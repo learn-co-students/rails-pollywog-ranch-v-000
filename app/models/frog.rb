a1636ad29dc87c173893280c17fa19c8e352703b
tttra <tothutra@gmail.com>
2016-07-17 17:00:35 -0400

    Done.

--- app/models/frog.rb
+++ app/models/frog.rb
@@ -1,3 +1,6 @@
 class Frog < ActiveRecord::Base
-  # code goes here
+  belongs_to :pond
+  has_many :tadpoles
+
+  validates_presence_of :name, :color
 end
--- app/models/pond.rb
+++ app/models/pond.rb
@@ -1,3 +1,6 @@
 class Pond < ActiveRecord::Base
-  # code goes here
+  has_many :frogs
+  has_many :tadpoles, through: :frogs
+
+  validates_presence_of :name, :water_type
 end
