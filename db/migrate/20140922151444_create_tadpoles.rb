50d7b17fc3bdd0fbe8fb951e2a2f8c593bc6ab22
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-30 10:09:54 -0400

    donzo

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,10 @@
 class CreateTadpoles < ActiveRecord::Migration
+  def change
+    create_table :tadpoles do |t|
+      t.integer :frog_id
+      t.string :name
+      t.string :color
+    end
+  end
   # code goes here
 end
