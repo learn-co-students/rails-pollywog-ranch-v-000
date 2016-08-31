425bcc0c867eaca249fc04d745f70297f5eeb123
Jesse Novotny <jessenovotny@gmail.com>
2016-08-31 16:43:51 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922151507) do
+ActiveRecord::Schema.define(version: 20160831232927) do
+
+  create_table "frogs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "pond_id"
+  end
+
+  create_table "ponds", force: :cascade do |t|
+    t.string   "name"
+    t.string   "water_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tadpoles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "frog_id"
+  end
 
 end
