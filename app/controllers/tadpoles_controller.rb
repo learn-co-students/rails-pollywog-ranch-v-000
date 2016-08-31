3cf77e976079d57cc9e8cc1c60b6b219e9e9e94f
steveafrost <ffrostt@gmail.com>
2016-08-30 22:04:08 -0400

    complete using a ribbiting metamorphosize action

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,7 +1,11 @@
 class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
-  # add your metamorphosize action here
+  def metamorphosize
+    @frog = Frog.create(:name => @tadpole.name, :color => @tadpole.color, :pond => @tadpole.pond)
+    @tadpole.destroy
+    redirect_to @frog
+  end
   
   def index
     @tadpoles = Tadpole.all
@@ -48,15 +52,16 @@ class TadpolesController < ApplicationController
   end
 
   private
-    def set_tadpole
-      @tadpole = Tadpole.find(params[:id])
-    end
 
-    def set_frog
-      @frog = Frog.find(params[:frog_id])
-    end
+  def set_tadpole
+    @tadpole = Tadpole.find(params[:id])
+  end
 
-    def tadpole_params
-      params.require(:tadpole).permit(:name, :color, :frog_id)
-    end
+  def set_frog
+    @frog = Frog.find(params[:frog_id])
+  end
+
+  def tadpole_params
+    params.require(:tadpole).permit(:name, :color, :frog_id)
+  end
 end
