b96971ad7441f499b55865d1f3b8f7de82e4c14e
jbok4 <jbok4@aol.com>
2016-09-09 12:29:28 -0400

    migrations complete

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,9 @@
 class CreatePonds < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :ponds do |t|
+      t.string :name
+      t.string :watertype
+      t.string :color
+    end
+  end
 end
