ed0ded12bf019bc649ba0ff585b5fdfbab939165
Kristian Kanya <kristian@nextgxdx.com>
2016-09-18 13:43:42 -0500

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,9 @@
 class Tadpole < ActiveRecord::Base
-  # code goes here
+  belongs_to :frog
+
+  # http://stackoverflow.com/questions/4021322/belongs-to-through-associations/11457714#11457714
+  # belongs to a pond through a frog behavior
+  # The Delegation Design Pattern is a technique where an object exposes certain behavior but 
+  # it actually delegates responsibility for implementing that behavior to an associated object.
+  delegate :pond, :to => :frog, :allow_nil => true
 end
