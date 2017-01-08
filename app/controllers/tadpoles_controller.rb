ef8733b12d942c98f3e9a7f5b72e39c3238f47a7
github username <github email address>
2017-01-08 18:01:00 -0500

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,8 +1,18 @@
 class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
-  # add your metamorphosize action here
-  
+  def metamorphosize
+    @frog = Frog.new(:color => @tadpole.color, :name => @tadpole.name, :pond_id => @tadpole.pond.id)
+    respond_to do |format|
+      if @frog.save
+        @tadpole.destroy
+        format.html { redirect_to @frog, notice: "#{@frog.name} the Tadpole successfully became a frog." }
+      else
+        format.html { render "/tadpoles/#{@tadpole.id}" }
+      end
+    end
+  end
+
   def index
     @tadpoles = Tadpole.all
   end
@@ -59,4 +69,5 @@ class TadpolesController < ApplicationController
     def tadpole_params
       params.require(:tadpole).permit(:name, :color, :frog_id)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   get '/ponds/:id/destroy', to: 'ponds#destroy'
   get '/frogs/:id/destroy', to: 'frogs#destroy'
   get '/tadpoles/:id/destroy', to: 'tadpoles#destroy'
-  
+
   # code your post to  '/tadpoles/:id/metamorphosize' here
+  post '/tadpoles/:id/metamorphosize', to: 'tadpoles#metamorphosize'
   # it should direct to the tadpoles controller, the metamorphosize action
 
   # resources
