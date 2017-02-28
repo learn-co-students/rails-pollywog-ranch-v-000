a40a868d0aff8fb19d644481f1361aa64e299530
Jon Fougner <jon.fougner@gmail.com>
2017-02-28 17:42:07 +0000

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,9 @@
 class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :frogs do |t|
+      t.belongs_to :pond
+      t.string :name
+      t.string :color
+    end
+  end
 end
--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,9 @@
 class CreateTadpoles < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :tadpoles do |t|
+      t.belongs_to :frog
+      t.string :name
+      t.string :color
+    end
+  end
 end
