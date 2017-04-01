8b7757fcaedbc83b4939003d801c292fcbfeea90
John Verdone <jmverdone@gmail.com>
2017-04-01 12:59:09 -0400

    Done.

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,10 @@
 class CreatePonds < ActiveRecord::Migration
+  def change
+    create_table :ponds do |t|
+      t.string :name
+      t.string :water_type
+      t.string :color
+    end
+  end
   # code goes here
 end
