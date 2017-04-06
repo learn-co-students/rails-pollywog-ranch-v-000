945891afed91c3704bf20379e133c75d36c66dc3
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-06 11:09:55 -0400

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,62 +1,71 @@
 class TadpolesController < ApplicationController
-  before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
+    before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
-  # add your metamorphosize action here
-  
-  def index
-    @tadpoles = Tadpole.all
-  end
-
-  def show
-  end
-
-  def new
-    @frog = Frog.find(set_frog)
-    @tadpole = Tadpole.new
-  end
-
-  def edit
-    @frog = @tadpole.frog
-  end
-
-  def create
-    @tadpole = Tadpole.new(tadpole_params)
-    respond_to do |format|
-      if @tadpole.save
-        format.html { redirect_to @tadpole, notice: 'Tadpole was successfully created.' }
-      else
-        format.html { render :new }
-      end
+    def metamorphosize
+        tadpole = Tadpole.find(params[:id])
+        @frog = Frog.create(my_attributes(tadpole))
+        tadpole.destroy
+        redirect_to frog_path(@frog)
     end
-  end
-
-  def update
-    respond_to do |format|
-      if @tadpole.update(tadpole_params)
-        format.html { redirect_to @tadpole, notice: 'Tadpole was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
+    
+    def index
+        @tadpoles = Tadpole.all
     end
-  end
 
-  def destroy
-    @tadpole.destroy
-    respond_to do |format|
-      format.html { redirect_to tadpoles_url, notice: 'Tadpole was successfully destroyed.' }
+    def show
     end
-  end
 
-  private
+    def new
+        @frog = Frog.find(set_frog)
+        @tadpole = Tadpole.new
+    end
+
+    def edit
+        @frog = @tadpole.frog
+    end
+
+    def create
+        @tadpole = Tadpole.new(tadpole_params)
+        respond_to do |format|
+            if @tadpole.save
+                format.html { redirect_to @tadpole, notice: 'Tadpole was successfully created.' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @tadpole.update(tadpole_params)
+                format.html { redirect_to @tadpole, notice: 'Tadpole was successfully updated.' }
+            else
+                format.html { render :edit }
+            end
+        end
+    end
+
+    def destroy
+        @tadpole.destroy
+        respond_to do |format|
+            format.html { redirect_to tadpoles_url, notice: 'Tadpole was successfully destroyed.' }
+        end
+    end
+
+    private
     def set_tadpole
-      @tadpole = Tadpole.find(params[:id])
+        @tadpole = Tadpole.find(params[:id])
     end
 
     def set_frog
-      @frog = Frog.find(params[:frog_id])
+        @frog = Frog.find(params[:frog_id])
     end
 
     def tadpole_params
-      params.require(:tadpole).permit(:name, :color, :frog_id)
+        params.require(:tadpole).permit(:name, :color, :frog_id)
+    end
+    
+    def my_attributes(tadpole)
+        {name: tadpole.name, color: tadpole.color, pond_id: tadpole.pond.id}
     end
 end
