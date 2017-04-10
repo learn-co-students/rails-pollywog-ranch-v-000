24c4800cbc90abadfa1ad4d67ab9cd5e7433151e
John Pfingst <jpfingst@outlook.com>
2017-04-10 16:31:53 +0000

    delegation and custom route

---
+++ db/migrate/20170410144941_create_tadpoles.rb
@@ -0,0 +1,11 @@
+class CreateTadpoles < ActiveRecord::Migration
+  def change
+    create_table :tadpoles do |t|
+      t.string :name
+      t.string :color
+      t.belongs_to :frog
+
+      t.timestamps null: false
+    end
+  end
+end
