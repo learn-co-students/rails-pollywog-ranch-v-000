b4af5037cc73e4ef5e7e4b3d8c94e638f0f725c3
github username pianistps <github email address pianistps@yahoo.com>
2017-04-18 19:59:18 -0500

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,6 +2,9 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
   # add your metamorphosize action here
+  def metamorphose
+
+  end
   
   def index
     @tadpoles = Tadpole.all
