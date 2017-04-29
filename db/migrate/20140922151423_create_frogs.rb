70164758aed73ce0c276a150c997eafbb478af68
Scott Bewick <bewick.scott@gmail.com>
2017-04-28 20:12:25 -0500

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,11 @@
 class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+  
+  def change 
+    create_table :frogs do |t|
+      t.string :color 
+      t.string :name
+      t.integer :pond_id
+    end  
+  end
+
 end
