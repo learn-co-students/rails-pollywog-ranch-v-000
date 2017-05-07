26fd25acf8e819f2be47c04c7acf6383f9687f94
Seth <itz.saga@gmail.com>
2017-05-06 23:06:42 -0500

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,11 @@
 class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :frogs do |t|
+      t.string :name
+      t.string :color
+      t.integer :pond_id
+
+      t.timestamps
+    end
+  end
 end
--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,11 @@
 class CreateTadpoles < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :tadpoles do |t|
+      t.string :name
+      t.string :color
+      t.integer :frog_id
+
+      t.timestamps
+    end
+  end
 end
