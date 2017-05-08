162b79421e5db3970f8782dc86655b3e2be18e98
kkashuda <kkashuda@gmail.com>
2017-05-08 13:10:13 -0500

    Done.

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,10 @@
 class CreateTadpoles < ActiveRecord::Migration
   # code goes here
+  def change 
+    create_table :tadpoles do |t| 
+      t.string :name 
+      t.string :color
+      t.string :frog_id 
+    end 
+  end 
 end
