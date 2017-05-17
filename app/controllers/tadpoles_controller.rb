c39634caf48edee93bd9cdc6d8bcada9a3d61e05
Damien de Lartigue <damien@delartigue.co.uk>
2017-05-17 17:54:45 +0000

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,7 +2,9 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
   # add your metamorphosize action here
-  
+  def metamorphosize
+  end
+
   def index
     @tadpoles = Tadpole.all
   end
