2b38fd02ac83ebfa587f920d5060954cba2b3590
Victoria <victoriamachen@gmail.com>
2017-07-15 13:10:44 -0500

    Delete 20140922151444_create_tadpoles.rb

--- db/migrate/20140922151444_create_tadpoles.rb
+++
@@ -1,9 +0,0 @@
-class CreateTadpoles < ActiveRecord::Migration
-  def change
-    create_table :tadpoles do |t|
-      t.string :name
-      t.string :color
-      t.integer :frog_id
-    end
-  end
-end
