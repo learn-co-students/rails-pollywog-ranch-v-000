a504a1ccbc318e08485e75965049fbd5d5a5b34a
Will-Sauer <will@heueso.com>
2017-07-31 12:09:04 -0700

    models passing

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,8 @@
 class Tadpole < ActiveRecord::Base
-  # code goes here
+  belongs_to :frog
+
+  def pond
+    self.frog.pond
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :tadpoles
+
+  resources :frogs
+
   # root
   root to: 'static_pages#home'
 
