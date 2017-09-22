074e7d5fee8dadff18a5a718adb83041d724d5c8
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-22 20:39:23 +0000

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,8 @@
 class Tadpole < ActiveRecord::Base
   # code goes here
+  belongs_to :frog
+  # has a frog_id
+  # So a tadpole belongs_to a frog, so it will have a foreign_key
+  delegate :pond, to: :frog, :allow_nil => true
+  # the pond_id and the frog_id allows the delegation
 end
