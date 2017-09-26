07e358c67701c29110d4790cf6b7eac5402a1214
leog7one <lguzman@me.com>
2017-09-25 22:08:50 -0500

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,10 @@
 class Tadpole < ActiveRecord::Base
   # code goes here
+  belongs_to :frog
+
+#belongs_to association cannot have a :through option
+#We must use delegate for the association of tadpoles belonging to a pond through a frog
+
+  delegate :pond, :to => :frog, :allow_nil => true
+
 end
