1d1bc261c3b8470acdc4cd216ff86a762485d453
hyoyou <heatherhyou@gmail.com>
2017-10-02 14:37:07 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   get '/ponds/:id/destroy', to: 'ponds#destroy'
   get '/frogs/:id/destroy', to: 'frogs#destroy'
   get '/tadpoles/:id/destroy', to: 'tadpoles#destroy'
-  
+
   # code your post to  '/tadpoles/:id/metamorphosize' here
   # it should direct to the tadpoles controller, the metamorphosize action
+  post '/tadpoles/:id/metamorphose', to: 'tadpoles#metamorphosize', as: 'metamorphose'
 
   # resources
   resources :ponds
