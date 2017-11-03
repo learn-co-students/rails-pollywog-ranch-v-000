2136774ee1514af3cac2ad2dd065ed40a41061f5
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-03 14:19:19 -0400

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,13 @@
 class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :frogs do |t|
+      t.string :name
+      t.string :color
+      t.integer :pond_id
+    end
+
+    add_index :frogs, :pond_id
+    add_foreign_key :books, :ponds
+  end
+
 end
--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,12 @@
 class CreateTadpoles < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :tadpoles do |t|
+      t.string :name
+      t.string :color
+      t.integer :frog_id
+    end
+
+    add_index :tadpoles, :frog_id
+    add_foreign_key :tadpoles, :frogs
+  end
 end
