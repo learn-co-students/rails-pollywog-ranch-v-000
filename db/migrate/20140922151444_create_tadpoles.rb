130c4bd403a679433eb5d56e40cd9438e39a0b02
ndalcin <dalcin.nicole@gmail.com>
2017-11-23 09:40:31 -0500

    Done.

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,12 @@
 class CreateTadpoles < ActiveRecord::Migration
+  def change
+    create_table :tadpoles do |t|
+      t.string :name
+      t.string :color
+      t.integer :frog_id
+
+      t.timestamps null: false
+    end
+  end
   # code goes here
 end
