1a14724f6906df68f6623e90519081a7a33a235a
EC2 Default User <ec2-user@ip-172-31-22-232.us-east-2.compute.internal>
2018-01-22 16:32:28 +0000

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,3 +1,7 @@
 class Tadpole < ActiveRecord::Base
   # code goes here
+  belongs_to :frog
+  delegate :pond, :to => :frog
+  validates :name, presence: :true
+  validates :color, presence: :true
 end
