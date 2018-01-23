c4346f5c9816c857a84eb04ec9ff340242b8bbf4
BenB <bblkmn5@gmail.com>
2018-01-23 09:59:36 -0800

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,9 @@
 class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :frogs do |c|
+      c.string :name
+      c.string :color
+      c.integer :pond_id
+    end
+  end
 end
