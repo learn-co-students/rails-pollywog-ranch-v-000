174d10b3b8cfb4b99b6038f47be37df5d8ec81b5
Chris Boujoukos <github email address>
2018-02-14 09:27:45 -0500

    Done.

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,12 @@
 class CreatePonds < ActiveRecord::Migration
-  # code goes here
+  def up
+    create_table :ponds do |t|
+      t.string :name
+      t.string :water_type
+    end
+  end
+
+  def down
+    drop_table :ponds
+  end
 end
