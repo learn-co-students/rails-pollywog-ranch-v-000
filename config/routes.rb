5a7f8549a2fc59decce6fdab88b84684c923bde6
Joshua Needham <joshuabneedham@gmail.com>
2018-02-24 01:12:29 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   # code your post to  '/tadpoles/:id/metamorphose' here
   # it should direct to the tadpoles controller, the metamorphose action
+  post '/tadpoles/:id/metamorphose', to: 'tadpoles#metamorphose', as: 'metamorphose'
 
   # resources
   resources :ponds
@@ -20,5 +21,4 @@ Rails.application.routes.draw do
   resources :frogs do
     resources :tadpoles, :only => [:new]
   end
-
 end
