b5a5cc8a3947d639e497ba5e202a4798bfcd2ddc
Autumn J <autumnkangas@gmail.com>
2018-03-01 13:50:27 -0600

    Done.

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,8 +2,10 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   def metamorphose
-    frog = Frog.new(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
-
+    #frog = Frog.new(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
+    @frog.name = @tadpole.name
+    @frog.color = @tadpole.color
+    @frog.pond = @tadpole.pond
     if frog.save
       @tadpole.destroy
       redirect_to frog, notice: "#{frog.name} the Tadpole successfully became a frog."
