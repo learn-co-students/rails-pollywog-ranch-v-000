16a4c9943fa3094b8f2deda1e18c8b690b0d13b8
jacobsilver2 <jacobsilver2@mac.com>
2018-04-03 08:07:26 -0400

    now onto the form.  did the method

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,7 +2,9 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   def metamorphose
-    
+    @frog = Frog.create(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond )
+    @tadpole.destroy
+    redirect_to @frog
   end
 
   def index
