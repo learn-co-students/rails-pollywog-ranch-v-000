061f0c08ee82ac276085f583f602220f1afc3dde
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-11 18:14:52 +0000

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,11 @@
 class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :frogs do |t|
+      t.string :name
+      t.string :color
+      t.belongs_to :pond
+
+      t.timestamps null: false
+    end
+  end
 end
