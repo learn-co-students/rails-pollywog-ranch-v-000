00ccdb1c4a226766e28940e908d11fb4ce0cc71c
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 19:24:41 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,3 @@
 Rails.application.routes.draw do
-
-  # root
-  root to: 'static_pages#home'
-
-  # destroy as get requests
-  get '/ponds/:id/destroy', to: 'ponds#destroy'
-  get '/frogs/:id/destroy', to: 'frogs#destroy'
-  get '/tadpoles/:id/destroy', to: 'tadpoles#destroy'
-
-  # code your post to  '/tadpoles/:id/metamorphose' here
-  # it should direct to the tadpoles controller, the metamorphose action
-
-  # resources
-  resources :ponds
-
-  resources :tadpoles, :only => [:index, :show, :edit, :update, :destroy, :metamorphose, :create]
-
-  # nested resources
-  resources :frogs do
-    resources :tadpoles, :only => [:new]
-  end
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
